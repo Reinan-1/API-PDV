5918b37e4a21361497b5c79379c51cee47784f54
unknown <facesofprosperidade@gamil.com>
2023-11-28 12:13:36 -0300

    feat: Configuração incial da API

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+
+const rotas = require("./rotas");
+
+const app = express();
+const porta = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(rotas);
+
+app.listen(porta);
